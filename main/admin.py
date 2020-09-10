58ebe724b12a167bbbc378f63568300ee62f24a7
Andrey Bogdanov <banananick19@mail.ru>
2020-09-10 16:52:20 +0300

    Добавлены модели 'Категория', 'Дочерная категория', 'Статья'

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import MODELS_FOR_ADMIN
 # Register your models here.
+
+for model in MODELS_FOR_ADMIN:
+    admin.site.register(model)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
 # Create your models here.
+
 
 class User(AbstractUser):
     pass
+
+
+class PrimaryCategory(models.Model):
+    label = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.label)
+
+
+class SecondaryCategory(models.Model):
+    parent_category = models.ForeignKey('PrimaryCategory', on_delete=models.PROTECT, null=True, blank=True)
+    label = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.parent_category.label) + '->' + str(self.label)
+
+
+class Article(models.Model):
+    label = models.CharField(max_length=100, null=True, blank=True)
+    text = models.TextField(default='Тут пусто...')
+    primary_category = models.ForeignKey('PrimaryCategory', on_delete=models.PROTECT, null=True, blank=True)
+    secondary_category = models.ForeignKey('SecondaryCategory', on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.label)
+
+
+MODELS_FOR_ADMIN = [User, PrimaryCategory, SecondaryCategory, Article]
